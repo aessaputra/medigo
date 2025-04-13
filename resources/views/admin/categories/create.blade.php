652171758416e8c2853a6def4ff0ef90ac8946cf
aessaputra <aessaputra@yahoo.com>
2025-04-13 15:02:33 +0000

    feat: Implement category listing and display in admin panel

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -1,21 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('New Category') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden p-10 shadow-sm sm:rounded-lg">
-
-                {{-- @if ($errors->any())
-                    @foreach ($errors->all() as $error)
-                        <div class="py w-full rounded-3xl bg-red-500 text-white">
-                            {{ $error }}
-                        </div>
-                    @endforeach
-                @endif --}}
 
                 <form method="POST" action="{{ route('admin.categories.store') }}" enctype="multipart/form-data">
                     @csrf
